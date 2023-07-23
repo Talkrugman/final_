471b9f9c2b4ace7d218ea320f28e9e72c8fd1d11
tal krugman <talkrugman@gmail.com>
2023-07-23 12:49:49 +0300

    new

--- src/Componnets/Navbar.jsx
+++ src/Componnets/Navbar.jsx
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return <nav>
-        gluten free home made products
+        <h1>gluten free home made products</h1>
         <ul>
             <li><Link to='/'>home</Link></li>
             <li><Link to='/products'>catalog</Link></li>
             <li><Link to='/cart'>cart</Link></li>
         </ul>
-        
+
     </nav>
 }
