b41131856da752d3ab99ecfd420a1f49c7f29fa8
tal krugman <talkrugman@gmail.com>
2023-07-24 19:03:47 +0300

    new

--- src/Componnets/Header.jsx
+++ src/Componnets/Header.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 export default function Header() {
     return <header className="bg-blue-500 py-4">
         <nav className="container mx-auto flex items-center justity-between px-4">
-            <a href="#" className="text-white text-lg font-semibold">My Website</a>
+            <a href="#" className="text-white text-lg font-semibold">Homemade Gluten Free Products</a>
             <ul className="flex space-x-4">
                 <li><a href="#" className="text-white hover:text-gray-200">Home</a></li>
                 <li><a href="#" className="text-white hover:text-gray-200">About</a></li>
