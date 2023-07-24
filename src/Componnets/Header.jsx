1dce84c11f8d879c98fa912a927db8f8eeb25b49
tal krugman <talkrugman@gmail.com>
2023-07-24 19:00:01 +0300

    new

--- src/Componnets/Header.jsx
+++ src/Componnets/Header.jsx
@@ -1,8 +1,19 @@
 import Navbar from "./Navbar";
 
 export default function Header() {
-    return <header>
-        <Navbar />
+    return <header className="bg-blue-500 py-4">
+        <nav className="container mx-auto flex items-center justity-between px-4">
+            <a href="#" className="text-white text-lg font-semibold">My Website</a>
+            <ul className="flex space-x-4">
+                <li><a href="#" className="text-white hover:text-gray-200">Home</a></li>
+                <li><a href="#" className="text-white hover:text-gray-200">About</a></li>
+                <li><a href="#" className="text-white hover:text-gray-200">Service</a></li>
+                <li><a href="#" className="text-white hover:text-gray-200">Content</a></li>
+            </ul>
+        </nav>
     </header>
+    {/* <header>
+        <Navbar />
+    </header> */}
 }
 
